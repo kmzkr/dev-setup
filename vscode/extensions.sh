d34f082ed527c7f7f11de604b551e46e899d25b1
kmzkr <kmzkrcom@gmail.com>
2025-02-04 22:21:46 +0900

    vscodeの拡張機能を追加

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -1,2 +1,7 @@
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension github.copilot
+code --install-extension github.copilot-chat
 code --install-extension hediet.vscode-drawio
 code --install-extension mhutchie.git-graph
+code --install-extension ms-ceintl.vscode-language-pack-ja
+code --install-extension ritwickdey.liveserver
